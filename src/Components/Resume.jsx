f29c61a96541f25e75bc349e9f643b360eefe415
Nikita711 <abhi9041355@gmail.com>
2020-08-30 17:27:57 +0530

    dummy pdf added

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import React, { Component } from "react";
+import "./Resume.css";
+// import { Document, Page } from "react-pdf";
 
-import samplePDF from "./Resume.pdf";
+// import samplePDF from "./Resume.pdf";
 
-function Resume() {
-  const [numPages, setNumPages] = useState(null);
+// function Resume() {
+//   const [numPages, setNumPages] = useState(null);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
+//   function onDocumentLoadSuccess({ numPages }) {
+//     setNumPages(numPages);
+//   }
+
+//   return (
+//     <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
+//       {Array.from(new Array(numPages), (el, index) => (
+//         <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+//       ))}
+//     </Document>
+//   );
+// }
+
+class Resume extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h2>Have a look at my Dummy resume </h2>
+        <p className="dummy">
+          <a href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf">
+            Click here
+          </a>
+        </p>
+      </>
+    );
   }
-
-  return (
-    <Document file={samplePDF} onLoadSuccess={onDocumentLoadSuccess}>
-      {Array.from(new Array(numPages), (el, index) => (
-        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-      ))}
-    </Document>
-  );
 }
+
 export default Resume;
